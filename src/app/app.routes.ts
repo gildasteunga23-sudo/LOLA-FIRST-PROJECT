7290b9b551ff45c240023f55f0ac27ba6a1dd001
mbemnova25-cloud <mbemnova25@gmail.com>
2026-09-18 15:31:51 +0100

    edithe soute

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,18 @@
 import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { Conflict } from './conflict/conflict';
+import { Confort } from './confort/confort';
 
 export const routes: Routes = [
+{path:'about',component:About},
+{path:'conflict',component:Conflict},
+{path:'confort',component:Confort},
+    
+
+
+
+
+
 
 
 ];
